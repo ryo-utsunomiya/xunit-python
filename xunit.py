0f8465d123869a3b8d4e3e5352b924a4d9c27826
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-10-15 00:51:08 +0900

    Init

---
+++ xunit.py
@@ -0,0 +1,27 @@
+class TestCase:
+    def __init__(self, name):
+        self.name = name
+
+    def run(self):
+        method = getattr(self, self.name)
+        method()
+
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.was_run = None
+        super().__init__(name)
+
+    def test_method(self):
+        self.was_run = 1
+
+
+class TestCaseTest(TestCase):
+    def test_running(self):
+        test = WasRun("test_method")
+        assert not test.was_run
+        test.run()
+        assert test.was_run
+
+
+TestCaseTest("test_running").run()
